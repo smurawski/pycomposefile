4e19e846535d30b3c7973db37efcbf07c1e29e56
Steven Murawski <steven.murawski@gmail.com>
2022-02-17 14:27:16 +0000

    Refactor __init__

--- pycomposefile/compose_element.py
+++ pycomposefile/compose_element.py
@@ -10,30 +10,40 @@ class ComposeElement:
     def __init__(self, config, compose_path=""):
         self.compose_path = compose_path
         for key in self.supported_keys.keys():
-            value = config.pop(key, None)
-            transform = self.supported_keys[key]
-            if isinstance(value, dict):
-                value = transform(key, value, compose_path)
-            elif value is not None:
-                if type(transform) is tuple:
-                    transform, valid_values = transform
-                    value = self.transform_incoming_data(value, transform, valid_values)
-                else:
-                    value = self.transform_incoming_data(value, transform)
-            self.__setattr__(key, value)
+            self.set_supported_property_from_config(config, compose_path, key)
         for key in self.unsupported_keys.keys():
-            message, docs_url, spec_url = self.unsupported_keys[key]
-            value = UnsupportedConfiguration(key, message, docs_url, spec_url, compose_path)
-            self.__setattr__(key, value)
+            self.set_unsupported_property_from_config(compose_path, key)
         for key in config.keys():
             if key not in self.unsupported_keys.keys():
-                pass
                 # raise Exception(f"Failed to map {key} in {compose_path}")
+                pass
 
-    def transform_incoming_data(self, value, data_transformer, valid_values=[]):
+    def set_unsupported_property_from_config(self, compose_path, key):
+        message, docs_url, spec_url = self.unsupported_keys[key]
+        value = UnsupportedConfiguration(key, message, docs_url, spec_url, compose_path)
+        self.__setattr__(key, value)
+
+    def set_supported_property_from_config(self, config, compose_path, key):
+        value = config.pop(key, None)
+        transform = self.supported_keys[key]
+        if isinstance(value, dict):
+            value = transform(key, value, compose_path)
+        elif value is not None:
+            value = self.transform_supported_data(value, transform)
+        self.__setattr__(key, value)
+
+    def transform_supported_data(self, value, transform):
+        if type(transform) is tuple:
+            transform, valid_values = transform
+            value = self.transform_and_validate_supported_data(value, transform, valid_values)
+        else:
+            value = transform(self.replace_environment_variables(value))
+        return value
+
+    def transform_and_validate_supported_data(self, value, data_transformer, valid_values):
         # TODO: if the data is an integer or decimal, should there be a "between" check?
         transformed = data_transformer(self.replace_environment_variables(value))
-        if len(valid_values) > 0 and transformed not in valid_values:
+        if transformed not in valid_values:
             # TODO: Should this return None or should this raise?
             return None
         else:
